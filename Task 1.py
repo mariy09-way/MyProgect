3f7da56110f692a61c7a73175426033c42a397fc
mariy09-way <mariyabalakshiy09@gmail.com>
2026-03-11 16:24:27 +0600

    Исправление Issues_1

--- Task 1.py
+++ Task 1.py
@@ -1,6 +1,7 @@
-name = input("Введите имя:")
-print("Привет,", name)
-userName = name
+name = input("Введите имя: ")
+family = input("Введите фамилию: ")
+userName = f"{name} {family}"
+print(f"Привет {userName}")
 userAge = 37
 user = f"name: {userName}  age: {userAge}"
 print(user)
